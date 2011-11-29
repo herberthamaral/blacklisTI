a2a2abd665299ac3e660da638d426921cd763635
Herberth Amaral <herberthamaral@gmail.com>
2011-11-29 16:27:28 -0200

    removendo app registration e mudando o nome de reviews

--- urls.py
+++ urls.py
@@ -2,22 +2,13 @@ from django.conf.urls.defaults import patterns, include, url
 import settings
 import home
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', home.views.index, name='home'),
-    # url(r'^blacklisti/', include('blacklisti.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    (r'^accounts/', include('registration.urls')),
-    (r'^accounts/profile/', include('profile.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^profile/', include('profile.urls')),
     (r'^review/', include('review.urls')),
 )
 
